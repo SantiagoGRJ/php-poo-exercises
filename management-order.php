4ca18b729e02672068856267b8f9c16cb28664c5
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2025-12-18 18:06:33 -0600

    feat: add management order

--- management-order.php
+++ management-order.php
@@ -1,6 +1,6 @@
 <?php
 
-enum OrderStatus: string {
+enum OrderStatus : string {
     case Pending = 'pending';
     case Paid = 'paid';
     case Cancelled = 'cancelled';
@@ -46,17 +46,21 @@ class Order {
         return $this->status;
     }
 
+    protected function setStatus($status){
+         $this->status=$status;
+    }
+
 
     public function pay(){
         if($this->status!==OrderStatus::Pending){
-            throw new InvalidArgumentException("The order cannot be paid");
+            throw new RuntimeException("Order can not be paid");
         }
         $this->status=OrderStatus::Paid;
     }
 
     public function cancel(){
         if($this->status!==OrderStatus::Pending){
-            throw new InvalidArgumentException("The order cannot be cancelled");
+            throw new RuntimeException("Order can not be cancelled");
         }
         $this->status=OrderStatus::Cancelled;
     }
@@ -65,29 +69,181 @@ class Order {
         return $this->status===OrderStatus::Pending;
     }
 
+    public function __toString() : string
+    {
+        return "Id: ". $this->id." Customer: ".$this->customerName." TotalAmount: ".$this->totalAmount." Status: ".$this->status->value;
+    }
+
 
 
 }
 
 class OnlineOrder extends Order {
 
-    protected $shoppingCost;
-    protected $total;
+    private $shippingCost;
+    
 
-    public function __construct(int $id, string $customerName, float $totalAmount,$shoppingCost)
+    public function __construct(int $id, string $customerName, float $totalAmount,float $shippingCost)
     {
-        $this->shoppingCost=$shoppingCost;
+        $this->shippingCost=$shippingCost;
         parent::__construct($id, $customerName, $totalAmount);
     }
 
-    protected function sumTotal(){
-        $this->total=$this->getTotalAmount() + $this->shoppingCost;
+    public function getTotalAmount(){
+       return  $this->totalAmount + $this->shippingCost;
     }
 
     public function cancel()
     {
-        if(!$this->canBeCancelled()){
-
+        if($this->status == OrderStatus::Paid){
+            throw new RuntimeException("Paid online order can not be Cancelled");
         }
+        parent::cancel();
     }
 }
+
+class InStoreOrder extends Order {
+
+    public function cancel()
+    {
+        if($this->status === OrderStatus::Cancelled){
+            throw new RuntimeException("The order is already cancelled");
+        }
+        $this->status=OrderStatus::Cancelled;
+    }
+
+    public function canBeCancelled()
+    {
+        return $this->getStatus() !== OrderStatus::Cancelled;
+    }
+    
+}
+
+class PickupOrder extends Order {
+
+    private $pickupLocation;
+
+    public function __construct(
+        int $id,
+        string $customerName,
+        float $totalAmount,
+        string $pickupLocation
+    ) {
+        $this->pickupLocation = $pickupLocation;
+        parent::__construct($id, $customerName, $totalAmount);
+    }
+
+    public function getPickupLocation(): string
+    {
+        return $this->pickupLocation;
+    }
+
+    public function pay()
+    {
+        if($this->status === OrderStatus::Cancelled){
+            throw new RuntimeException("Cancelled order can not be paid");
+        }
+        parent::pay();
+    }
+
+    public function canBeCancelled()
+    {
+        return $this->getStatus() !== OrderStatus::Paid ;
+    }
+}
+
+class OrderManager {
+    private array $orders = [];
+
+    public function addOrder(Order $order){
+        $this->validateId($order->getId());
+        $this->orders[] = $order;
+    }
+
+    public function findOrderById(int $id){
+        foreach($this->orders as $order){
+            if($order->getId() === $id){
+                return $order;
+            }
+        }
+        return null;
+    }
+
+    public function payOrder(int $id){
+         $order=$this->findOrderById($id);
+           if (!$order) {
+        throw new RuntimeException("Order not found");
+        }
+         $order->pay();
+
+    }
+
+    public function cancelOrder(int $id){
+        $order=$this->findOrderById($id);
+         if (!$order) {
+        throw new RuntimeException("Order not found");
+    }
+        $order->cancel();
+    }
+
+    public function listOrders(){
+        $text=" \n";
+        foreach($this->orders as $order){
+            $text.=$order." \n";
+        }
+        return $text;
+    }
+
+    public function getTotalPaidRevenue(){
+        $total=0;
+        foreach($this->orders as $order){
+            if($order->getStatus() === OrderStatus::Paid){
+                $total+=$order->getTotalAmount();
+            }
+        };
+        return $total;
+    }
+
+    private function validateId(int $id){
+         if($this->findOrderById($id)){
+            throw new RuntimeException("Order not found");
+         }
+    }
+}
+
+$order = new OrderManager;
+
+$order->addOrder(new OnlineOrder(1, "Santiago", 100, 20));
+$order->addOrder(new InStoreOrder(2, "Javier", 50));
+$order->addOrder(new PickupOrder(3, "Gomez", 80,"Mexico DF"));
+
+
+echo $order->listOrders();
+
+$order->payOrder(1);
+$order->payOrder(2);
+
+try {
+    $order->cancelOrder(1);
+} catch (RuntimeException $e) {
+    echo $e->getMessage() . "\n";
+}
+
+$order->cancelOrder(2);
+
+try {
+    $order->payOrder(2);
+} catch (RuntimeException $e) {
+    echo $e->getMessage() . "\n";
+}
+
+$order->cancelOrder(3);
+
+try {
+    $order->payOrder(3);
+} catch (RuntimeException $e) {
+    echo $e->getMessage() . "\n";
+}
+
+echo $order->listOrders();
+echo $order->getTotalPaidRevenue();
