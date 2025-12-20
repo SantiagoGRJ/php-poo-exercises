9e26892ac5006c42162a61e5bd295bef10836300
Santiago GRJ <santiago-2003-rojas@hotmail.com>
2025-12-19 22:30:58 -0600

    feat: add management Library

--- management-library/management-library.php
+++ management-library/management-library.php
@@ -1,12 +1,14 @@
 <?php
 
 class Book {
+
+    private bool $isAvailable = true;
     
     public function __construct(
         private int $id,
         private string $title,
         private string $author,
-        private bool $isAvailable = true,
+        
     )
     {}
 
@@ -45,14 +47,16 @@ class Book {
 
 class Member {
 
-    
+    private int $activeLoans;
    
     public function __construct( 
         private int $id,
         private string $name,
-        private int $activeLoans,
+        
     )
-    {  }
+    {
+        $this->activeLoans=0;
+    }
 
     public function getId(){
         return $this->id;
@@ -72,7 +76,7 @@ class Member {
 
     public function addLoan(){
         if(!$this->canBorrow()){
-            return false;
+            throw new RuntimeException("Member can not borrow more book, there is limited");
         }
         $this->activeLoans++;
     }
@@ -90,40 +94,50 @@ class Member {
 
 class Loan {
 
-    public function __construct(
-       private int $id,
-        private Book $book,
-        private Member $member,
-        private DateTime $startDate,
-        private ?DateTime $endDate = null,
-    ) {}
+    private static int $autoIncrement = 1;
 
-    public function getId() {
+    private int $id;
+    private Book $book;
+    private Member $member;
+    private DateTime $startDate;
+    private ?DateTime $endDate = null;
+
+    public function __construct(Book $book, Member $member){
+        $this->id = self::$autoIncrement++;
+        $this->book = $book;
+        $this->member = $member;
+        $this->startDate = new DateTime();
+    }
+
+    public function getId(): int {
         return $this->id;
     }
 
-    public function getBook() {
+    public function getBook(): Book {
         return $this->book;
     }
 
-    public function getMember() {
+    public function getMember(): Member {
         return $this->member;
     }
 
-    public function getStartDate() {
+    public function getStartDate(): DateTime {
         return $this->startDate;
     }
 
-    public function getEndDate() {
+    public function getEndDate(): ?DateTime {
         return $this->endDate;
     }
 
-    public function isActive() {
+    public function isActive(): bool {
         return $this->endDate === null;
     }
 
-    public function close() {
-        return $this->endDate = new DateTime();
+    public function close(): void {
+        if(!$this->isActive()){
+            throw new RuntimeException("Loan already closed");
+        }
+        $this->endDate = new DateTime();
     }
 }
 
@@ -164,13 +178,52 @@ class Library {
     public function borrowBook(int $bookId, int $memberId){
         $book=$this->findBookById($bookId);
         $member=$this->findMemberById($memberId);
+        $loan = new Loan($book,$member);
+        $this->loans[] = $loan;
+        $book->markAsBorrowed();
+        $member->addLoan($loan);
     }
 
     public function returnBook(int $bookId){
-
+        foreach($this->loans as $loan){
+        if($loan->getBook()->getId() === $bookId && $loan->isActive()){
+            $loan->close();
+            $loan->getBook()->markAsReturned();
+            $loan->getMember()->removeLoan($loan);
+            return;
+        }
+    }
     }
 
     public function getActiveLoans(){
-
+         $active = [];
+    foreach($this->loans as $loan){
+        if($loan->isActive()){
+            $active[] = $loan;
+        }
+    }
+    return $active;
     }
 }
+
+$library = new Library();
+
+$book1 = new Book(1, "Clean Code","santi");
+$book2 = new Book(2, "Design Patterns","javi");
+
+$member1 = new Member(1, "Santiago");
+$member2 = new Member(2, "Javier");
+
+$library->addBook($book1);
+$library->addBook($book2);
+
+$library->addMember($member1);
+$library->addMember($member2);
+
+$library->borrowBook(1, 1);
+$library->borrowBook(2, 2);
+
+$library->returnBook(1);
+
+$activeLoans = $library->getActiveLoans();
+var_dump($activeLoans);
